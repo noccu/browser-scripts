4eeebffc57e5cc081646acfd6af3678cc2ed61b6
noccu <37862870+noccu@users.noreply.github.com>
2025-12-19 23:45:00 +0100

    Refactor feed thumb URLs to be more generic
    In anticipation of new features.

--- youtube_thumb_links.user.js
+++ youtube_thumb_links.user.js
@@ -3,40 +3,51 @@
 // @namespace   yt_fuckery
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.2
+// @version     1.2.1
 // @author      noccu
 // @description Add thumbnail link to videos. Check the … menu.
 // ==/UserScript==
 
-function getThumb(el) {
+const [PREVIEW_BUTTON, PREVIEW_LINK] = createPreviewLink()
+var PREVIEW_ADDED = false
+
+
+function getPreviewThumbUrl(el) {
     thumbEl = el.querySelector("img.ytCoreImageHost")
     return thumbEl.src.split("?")[0].replace("hqdefault", "hq720")
 }
 
-const THUMB_BUTTON = document.createElement("yt-list-item-view-model")
-const THUMB_LINK = document.createElement("a")
-THUMB_LINK.textContent = "Thumbnail"
-THUMB_BUTTON.style.color = "inherit"
-THUMB_BUTTON.className = "yt-list-item-view-model yt-list-item-view-model__container"
-THUMB_LINK.className = "yt-list-item-view-model__title"
-THUMB_BUTTON.append(THUMB_LINK)
-var BUTTON_ADDED = false
-
-function addButton() {
-    if (BUTTON_ADDED) return
-    document.querySelector("yt-download-list-item-view-model").before(THUMB_BUTTON)
-    BUTTON_ADDED = true
+// Small thumbnails in a list/feed.
+function createPreviewLink() {
+    const btn = document.createElement("yt-list-item-view-model")
+    btn.style.color = "inherit"
+    btn.className = "yt-list-item-view-model yt-list-item-view-model__container"
+    const link = document.createElement("a")
+    link.textContent = "Thumbnail"
+    link.className = "yt-list-item-view-model__title"
+    btn.append(link)
+    return [btn, link]
 }
 
-function updateThumbUrl(e) {
+function addPreviewBtn() {
+    if (PREVIEW_ADDED) return
+    document.querySelector("yt-download-list-item-view-model").before(PREVIEW_BUTTON)
+    PREVIEW_ADDED = true
+}
+
+// Try to add a thumb link to feed menus.
+function updatePreviewUrl(e) {
+    const videoRoot = e.target.closest("div.yt-lockup-view-model")
+    if (videoRoot) {
+        addPreviewBtn()
+        PREVIEW_LINK.href = getPreviewThumbUrl(videoRoot)
+    }
+}
+
+function tryAddButtons(e) {
     if (e.target.tagName != "DIV" || !e.target.className.endsWith("fill")) {
         return
     }
-    const videoRoot = e.target.closest("div.yt-lockup-view-model")
-    if (videoRoot) {
-        addButton()
-        THUMB_LINK.href = getThumb(videoRoot)
-    }
+    updatePreviewUrl(e)
 }
-
-document.body.addEventListener("click", updateThumbUrl)
+document.body.addEventListener("click", tryAddButtons)
